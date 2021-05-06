05e90cc9c743219d644410809e97dffd07d8d5a8
Avinash Agarwal <avinash.agarwal@philips.com>
2021-05-07 00:59:21 +0530

    Adding models and creating superuser

--- mailerList_application/admin.py
+++ mailerList_application/admin.py
@@ -1,3 +1,6 @@
 from django.contrib import admin
+from .models import mail_History, mailId_List
 
 # Register your models here.
+admin.site.register(mail_History)
+admin.site.register(mailId_List)
--- mailerList_application/models.py
+++ mailerList_application/models.py
@@ -1,16 +1,17 @@
 from django.db import models
+from enum import Enum 
+
+class ProgressInfoEnum(Enum):
+    INITIAL = "Initial"
+    INPROGRESS = "In-progress"
+    SUCCESS = "Success"
+    FAILURE = "Failure"
 
 class mailId_List(models.Model):
     email_id = models.EmailField()
 
 class mail_History(models.Model):
-    mail_subject = models.CharField()
+    mail_subject = models.CharField(max_length=100)
     mail_content = models.TextField()
-    progress = models.CharField() #enum 
-
-'''
-enum : initial(default), in-progress, sucess, failure
-event based programming -> 
-
-'''
+    progress = models.CharField(max_length=11 , choices=[(tag, tag.value) for tag in ProgressInfoEnum], default=ProgressInfoEnum.INITIAL)
 
